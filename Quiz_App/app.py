b35fac1d0b0e85ae89fc904f740ec19c9129021b
Patilmanav <139691635+Patilmanav@users.noreply.github.com>
2024-05-27 18:29:04 +0530

    All Chapter ETI Added

--- Quiz_App/app.py
+++ Quiz_App/app.py
@@ -1,41 +1,95 @@
 from flask import Flask, render_template, request, redirect, url_for, session,jsonify
 import pandas as pd
 from Quiz_App import dbHandler
+import random
 
 app = Flask(__name__,template_folder='templates')
 app.secret_key = 'your_secret_key'  # Needed for session management
 
-questions = []
+ETIChapter1 = ['ETIchp1.csv']
+ETIChapter2 = ['ETIchp2.csv']
+ETIChapter3 = ['ETIchp3.csv']
+ETIChapter4 = ['ETIchp4.csv']
+ETIChapter5 = ['ETIchp5.csv']
+ETIChapter6 = ['ETIchp6.csv']
+ETIAllChapters = []
+questions = list()
+# Sample questions
+chplist = [ETIChapter1,ETIChapter2,ETIChapter3,ETIChapter4,ETIChapter5,ETIChapter6]
+for ch in chplist:
+    # ch2questions.clear()
+    try:
+        df = pd.read_csv(f'Quiz_App/QuestionDB/{ch[0]}', index_col=False)
+        # print(list(df.iterrows())[0][1]['Questions'])
 
-@app.route('/',methods=['GET', 'POST'])
+        for i, row in df.iterrows():
+            d = {'question': row['Questions'].replace('\t', ''),
+                 'choices': row['Options'].split(' | '),
+                  'answer': str(row['Answer']).replace('\t', '')}
+            ch.append(d)
+    except Exception as e:
+        print("Error reading CSV:",e)
+        ch.clear()
+        print(ETIChapter1)
+    else:
+        del ch[0]
+        print(ch)
+
+ETIAllChapters = ETIChapter1+ETIChapter2+ETIChapter3+ETIChapter4+ETIChapter5+ETIChapter6
+# ...........................
+@app.route('/', methods=['GET','POST'])
 def index():
-    # Sample questions
-    questions.clear()
-    df = pd.read_csv('Quiz_App/QuestionDB/ETIchp2.csv', index_col=False)
-    print(list(df.iterrows())[0][1]['Questions'])
-
-    for i, row in df.iterrows():
-        d = {'question': row['Questions'].replace('\t', ''),
-             'choices': [row['op1'].replace('\t', ''), row['op2'].replace('\t', ''), row['op3'].replace('\t', ''),
-                         row['op4'].replace('\t', '')], 'answer': str(row['ans']).replace('\t', '')}
-        questions.append(d)
-
     session['current_question'] = 0
     session['score'] = 0
     session['answers'] = []
     session['uName'] = ""
     session['QuesAddName'] = ""
     session['Qtype'] = ""
-    session['total_questions'] = len(questions)
     if request.method == 'POST':
-        return redirect(url_for('quiz'))
-    
-    # else:
-    #     return redirect(url_for('add_que'))
+        subject = request.form['subject']
+        chapter = request.form['chapter']
+        return redirect(url_for('No_of_Que',subject = subject,chapter=chapter))
     return render_template('index.html')
+
+@app.route('/No_of_Que/<subject>/<chapter>',methods=['GET', 'POST'])
+def No_of_Que(subject,chapter):
+
+
+    print(subject)
+    print(chapter)
+
+    chp = ['Chapter1', 'Chapter2', 'Chapter3', 'Chapter4', 'Chapter5', 'Chapter6', 'AllChapters']
+    d = {'ETIChapter1': ETIChapter1, 'ETIChapter2': ETIChapter2, 'ETIChapter3': ETIChapter3, 'ETIChapter4': ETIChapter4,
+         'ETIChapter5': ETIChapter5, 'ETIChapter6': ETIChapter6,'ETIAllChapters':ETIAllChapters}
+    if subject == 'MAN':
+        return "<h1>Database ERROR: Management MCQ's are not updated yet!!"
+
+    if d[subject + chapter] == []:
+        return f"{chapter} Database not updated yet!!"
+
+    if request.method == 'POST':
+        num_questions = request.form.get('num_questions')
+        print(num_questions)
+        if subject == 'ETI':
+            if chapter in chp:
+                try:
+                    global questions
+                    questions = random.sample(d[subject + chapter],int(num_questions))
+                except Exception as e:
+                    print(f'{num_questions}: {e}')
+                    return "Please Enter Valid Number"
+                if questions == []:
+                    return f"{chapter} Database not updated yet!!"
+                session['total_questions'] = len(questions)
+                print(questions)
+                return redirect(url_for('quiz'))
+        elif subject == 'MAN':
+            return "<h1>Database ERROR: Management MCQ's are not updated yet!!"
+    return render_template('NumOfQue.html',subject = subject,chapter=chapter)
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+
     current_question = session.get('current_question', 0)
     score = session.get('score', 0)
     answer = session.get('answers',[])
@@ -51,7 +105,7 @@ def quiz():
                 session['uName'] = request.form.get('name')
             isNameEntered = 1
 
-        if selected_choice == correct_answer:
+        if selected_choice.strip().split(".")[0] in correct_answer.strip().replace("Ans:",""):
             session['score'] = score + 1
         
         answer.append({
@@ -65,9 +119,10 @@ def quiz():
         
         if current_question + 1 >= len(questions):
             return redirect(url_for('results'))
-    
+    print("current_question = ",current_question,len(questions))
     if current_question < len(questions):
         question = questions[session['current_question']]
+        print("Questions are: ",question)
         return render_template('quiz.html',isNameEntered = isNameEntered ,question=question, current_question=current_question, total_questions=session['total_questions'])
     else:
         return redirect(url_for('results'))
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from Quiz_App.app import app
+import os
+# print(os.listdir(os.curdir))
 
 if __name__ == '__main__':
     app.run(debug=True)
